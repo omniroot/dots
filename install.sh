2b0eec86f4b017991b35cd79d436a5e9b7b02898
omniroot <omnirootofc@gmail.com>
2025-05-15 01:37:53 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@
 
 stow home/ -t ~/
 stow config/ -t ~/.config/
+stow local/ -t ~/.local/
+stow udev/ -t /etc/udev/rules.d/
+
+sudo chmod +x ~/.local/scripts/*
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
